a49e09bfe45d075edcb4dade261214d502f3f905
Philipp Weißmann <mail@philipp-weissmann.de>
2018-12-21 18:43:34 +0100

    Use pip-review for pip reviews

--- plugins.d/pip3.sh
+++ plugins.d/pip3.sh
@@ -14,37 +14,67 @@ function repair_pip_rights {
     fi
 }
 
+function install_pip_review {
+    if which pip-review >/dev/null 2>/dev/null; then
+        pip3 install pip-review
+    fi
+}
+
+function install_pip_review_sudo {
+    if which pip-review >/dev/null 2>/dev/null; then
+        sudo -H pip3 install pip-review
+    fi
+}
+
+function update {
+    pip-review --interactive
+}
+
+function update_sudo {
+    sudo -H pip-review --interactive
+}
+
 function list_outdated {
     pip3 list --outdated --format=freeze --local | grep -v '^\-e'
 }
 
-function pip_update {
-    if [[ $(list_outdated | grep -v "pep517") ]]; then
-        list_outdated | grep -v "pep517" | cut -d = -f 1  | xargs -n1 pip3 -q install -U
-    fi
-}
+# function pip_update {
+#     if [[ $(list_outdated | grep -v "pep517") ]]; then
+#         list_outdated | grep -v "pep517" | cut -d = -f 1  | xargs -n1 pip3 -q install -U
+#     fi
+# }
+#
+# function pip_update_osx {
+#     if [[ $(list_outdated | grep -v "pygobject") ]]; then
+#         list_outdated| grep -v "pygobject" | cut -d = -f 1  | xargs -n1 pip3 -q install -U
+#     fi
+# }
 
-function pip_update_osx {
-    if [[ $(list_outdated | grep -v "pygobject") ]]; then
-        list_outdated| grep -v "pygobject" | cut -d = -f 1  | xargs -n1 pip3 -q install -U
-    fi
-}
-
-function pip_update_sudo {
-    sudo -H pip3 install -U $(sudo -H pip3 freeze | grep -v pep517 | cut -d '=' -f 1) | grep -v "Requirement already up-to-date"
-}
+# function pip_update_sudo {
+#     sudo -H pip3 install -U $(sudo -H pip3 freeze | grep -v pep517 | cut -d '=' -f 1) | grep -v "Requirement already up-to-date"
+# }
 
 
 if which pip3 >/dev/null 2>/dev/null; then
     echo "🐍 Update pip3"
     if [[ "$OSTYPE" == "darwin"* ]]; then
-        if pip_update_osx; then
-            echo ""
+        install_pip_review_sudo
+        if update; then
+             echo ""
         else
             repair_pip_rights
-            pip_update_osx
+            update
         fi
+
+        # if pip_update_osx; then
+        #     echo ""
+        # else
+        #     repair_pip_rights
+        #     pip_update_osx
+        # fi
     else
-        pip_update_sudo
+        install_pip_review_sudo
+        # pip_update_sudo
+        update_sudo
     fi
 fi
