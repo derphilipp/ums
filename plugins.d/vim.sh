aee28c2a4227f121ff5336aa8e33eba968e62846
Philipp Weißmann <mail@philipp-weissmann.de>
2018-12-21 16:24:34 +0100

    Add newlines to vim script

--- plugins.d/vim.sh
+++ plugins.d/vim.sh
@@ -3,7 +3,9 @@
 if which nvim >/dev/null 2>/dev/null; then
     echo "📝 update nvim plugins"
     nvim --headless +"call dein#install()" +"call dein#update()" +UpdateRemotePlugins +qall
+    echo ""
 elif which vim >/dev/null 2>/dev/null; then
     echo "📝 update vim plugins"
     vim --headless +"call dein#install()" +"call dein#update()" +UpdateRemotePlugins +qall
+    echo ""
 fi
