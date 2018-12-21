c801158ec656372b51dd0503e840bc7217cc6c24
Philipp Weißmann <mail@philipp-weissmann.de>
2018-12-21 15:28:42 +0100

    Add emoji to mr

--- plugins.d/mr.sh
+++ plugins.d/mr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if which mr >/dev/null 2>/dev/null; then
-    echo "Update mr"
+    echo "📦 Update mr"
     mr -m -q -s --directory $HOME co && \
     mr -m -q -s --directory $HOME update
 fi
