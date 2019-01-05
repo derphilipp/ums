cfbd213a758c5aa147d6f0174dd2b6f1385a35a0
Philipp Weißmann <mail@philipp-weissmann.de>
2019-01-05 20:53:57 +0100

    Minor fixes on mr

--- plugins.d/mr.sh
+++ plugins.d/mr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if which mr >/dev/null 2>/dev/null; then
+if command -v mr >/dev/null 2>/dev/null; then
     echo "📦 Update mr"
-    mr -m -q -s --directory $HOME co && \
-    mr -m -q -s --directory $HOME update
+    mr -m -q -s --directory "$HOME" co && \
+    mr -m -q -s --directory "$HOME" update
 fi
