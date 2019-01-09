e8db0105aab873925409902af9deff144c02828b
Philipp Weißmann <mail@philipp-weissmann.de>
2019-01-09 12:02:28 +0100

    Fix pihole updater

--- plugins.d/pihole.sh
+++ plugins.d/pihole.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-if command -v mr >/dev/null 2>/dev/null; then
+if command -v pihole >/dev/null 2>/dev/null; then
     echo "📛 Update pihole"
     pihole -up
 fi
