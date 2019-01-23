3868fc7ac0e0c64545aecfbf39257017d36cfe3d
Philipp Weißmann <mail@philipp-weissmann.de>
2019-01-23 10:35:01 +0100

    Performs yarn clean always - it is fast & painless

--- plugins.d/yarn.sh
+++ plugins.d/yarn.sh
@@ -4,10 +4,6 @@ set -Eeuo pipefail
 if command -v yarn 2>/dev/null; then
   echo "📦  Yet another Yarn upgrade running ..."
   yarn global upgrade -s
-  echo ""
-
-  if [[ $1 == "cleanup" ]]; then
-    echo "🌬   Cleaning yarn cache"
-    yarn cache clean
-  fi
+  echo "🌬   Cleaning yarn cache"
+  yarn cache clean
 fi
