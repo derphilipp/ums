a25c198389780aa9d84c4c414b27cfe009cbc1db
Philipp Weißmann <mail@philipp-weissmann.de>
2019-04-10 10:59:52 +0200

    Only try updates if 'dein' plugin is installed

--- plugins.d/vim.sh
+++ plugins.d/vim.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-if command -v nvim >/dev/null 2>/dev/null; then
-    echo "📝 update nvim plugins"
-    nvim --headless +"call dein#install()" +"call dein#update()" +UpdateRemotePlugins +qall
-    echo ""
-elif command -v vim >/dev/null 2>/dev/null; then
-    echo "📝 update vim plugins"
-    vim --headless +"call dein#install()" +"call dein#update()" +UpdateRemotePlugins +qall
-    echo ""
+if [ -d  ~/.vim/dein/ ]; then
+    if command -v nvim >/dev/null 2>/dev/null; then
+        echo "📝 update nvim plugins"
+        nvim --headless +"call dein#install()" +"call dein#update()" +UpdateRemotePlugins +qall
+        echo ""
+    elif command -v vim >/dev/null 2>/dev/null; then
+        echo "📝 update vim plugins"
+        vim --headless +"call dein#install()" +"call dein#update()" +UpdateRemotePlugins +qall
+        echo ""
+    fi
 fi
